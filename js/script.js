23275ebc7ca121496484af348e5aa0039ec26683
Will Moritz <will@crowdfavorite.com>
2016-05-24 09:49:32 -0400

    addded images, js, audio, css template

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+  // Will's JS
+  var audio = new Audio('audio/chacarron.mp3');
+
+  $('#will-moritz').mouseenter(function() {
+    audio.play();
+  }).mouseleave(function(){
+    audio.pause();
+  });
+
+  // Anais Perez's Javascript
+
+
+  // Austin Yates's Javascript
+
+
+  // Hernan Rodriguez's Javascript
+
+
+  // Hilda Ortega's Javascript
+
+
+  // Jenifer Soriano's Javascript
+
+
+  // Jessica Valcin's Javascript
+
+
+  // Joel Colon's Javascript
+
+
+  // Johan Pena's Javascript
+
+
+  // Kenia Martinez's Javascript
+
+
+  // Luis Abreu's Javascript
+
+
+  // Pierre-Ryan Baptiste's Javascript
+
+
+  // Prince Warner's Javascript
+
+
+  // Roberto Roman's Javascript
+
+
+  // Sergio Hugo's Javascript
+
+
+  // Shyanne Jordan's Javascript
+
+
+  // Tyrese Francis's Javascript
+
+
+
+});
