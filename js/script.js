75bbec885cce6b70ef4445dff921b7002e784915
Your Name <you@example.com>
2016-05-26 16:07:42 -0400

    sup

--- js/script.js
+++ js/script.js
@@ -45,7 +45,9 @@ $(document).ready(function() {
   // Prince Warner's Javascript
   $('#prince-warner').mouseenter(function() {
     document.body.style.backgroundColor = "black";
-  })
+  }) .mouseleave(function(){
+     document.body.style.backgroundColor = "white";
+  });
 
   // Roberto Roman's Javascript
 
