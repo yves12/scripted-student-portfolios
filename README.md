807d58abd0b80efe6e3ed44019587d832c059834
Bijesse <tom@bijesse.com>
2016-06-08 14:31:53 -0400

    removed class specific content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# scripted-student-portfolios
+# ScriptEd Student Portfolios
+
+To see this website live go to [https://cdn.rawgit.com/Bijesse/scripted-student-portfolios/master/index.html](https://cdn.rawgit.com/Bijesse/scripted-student-portfolios/master/index.html)
--- js/script.js
+++ js/script.js
@@ -8,64 +8,12 @@ $(document).ready(function() {
   }).mouseleave(function(){
     audio.pause();
   });
- 
- var Fetty = new Audio('audio/Fetty Wap - RGF Island [Audio Only].mp3');
-
-  $('#Johan_Pena').mouseenter(function() {
-    Fetty.play();
-  }).mouseleave(function(){
-    Fetty.pause();
-  });
-  // Anais Perez's Javascript
 
 
-  // Austin Yates's Javascript
 
 
-  // Hernan Rodriguez's Javascript
 
 
-  // Hilda Ortega's Javascript
-
-
-  // Jenifer Soriano's Javascript
-
-
-  // Jessica Valcin's Javascript
-
-
-  // Joel Colon's Javascript
-
-
-  // Johan Pena's Javascript
-
-
-  // Kenia Martinez's Javascript
-
-
-  // Luis Abreu's Javascript
-
-
-  // Pierre-Ryan Baptiste's Javascript
-
-
-  // Prince Warner's Javascript
-  $('#prince-warner').mouseenter(function() {
-    document.body.style.backgroundColor = "black";
-  }) .mouseleave(function(){
-     document.body.style.backgroundColor = "white";
-  });
-
-  // Roberto Roman's Javascript
-
-
-  // Sergio Hugo's Javascript
-
-
-  // Shyanne Jordan's Javascript
-
-
-  // Tyrese Francis's Javascript
 
 
 
